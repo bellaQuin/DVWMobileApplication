41e39a1311003431b8ec054d6f577dca28508d30
NonStopPings <henr28@manukaumail.com>
2019-11-26 13:38:36 +1300

    26/11/2019

--- app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/Slide_12_Behavioural.java
+++ app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/Slide_12_Behavioural.java
@@ -9,6 +9,9 @@ import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.coachingapp.Issues_Problems.IP_Question_10;
+import com.example.coachingapp.Issues_Problems.IP_Question_13;
+import com.example.coachingapp.Models.Issues_13;
 import com.example.coachingapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,7 +27,7 @@ public class Slide_12_Behavioural extends AppCompatActivity {
 
     LinearLayout mLayout;
 
-    CheckBox myCheckBox;
+    CheckBox check_1,check_2,check_3,check_4,check_5,check_6,check_7,check_8,check_9,check_10,check_11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +41,67 @@ public class Slide_12_Behavioural extends AppCompatActivity {
         myRef = FirebaseDatabase.getInstance().getReference();
         database = FirebaseDatabase.getInstance();
 
-        myCheckBox = (CheckBox) findViewById(R.id.check_1);
-        myCheckBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(myCheckBox.isChecked()){
-                    Toast.makeText(getApplicationContext(),"Checked",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        check_1 = (CheckBox) findViewById(R.id.check_1);
+        check_2 = (CheckBox) findViewById(R.id.check_2);
+        check_3 = (CheckBox) findViewById(R.id.check_3);
+        check_4 = (CheckBox) findViewById(R.id.check_4);
+        check_5 = (CheckBox) findViewById(R.id.check_5);
+        check_6 = (CheckBox) findViewById(R.id.check_6);
+        check_7 = (CheckBox) findViewById(R.id.check_7);
+        check_8 = (CheckBox) findViewById(R.id.check_8);
+        check_9 = (CheckBox) findViewById(R.id.check_9);
+        check_10 = (CheckBox) findViewById(R.id.check_10);
+        check_11= (CheckBox) findViewById(R.id.check_11);
 
     }
 
 
     public void btn_next(View view) {
-        Intent intent = new Intent(Slide_12_Behavioural.this, Slide_12_Physical.class );
-        startActivity(intent);
+        addToFirebase();
+    }
+
+    private void addToFirebase() {
+
+        if (user != null) {
+            String id = user.getUid();
+
+            if(check_1.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Constantly worrying");
+            }
+            if(check_2.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Avoiding situations that are");
+            }
+            if(check_3.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Troubling you");
+            }
+            if(check_4.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Snapping at people");
+            }
+            if(check_5.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Biting your nails");
+            }
+            if(check_6.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Picking at your skin");
+            }
+            if(check_7.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Unable to concentrate");
+            }
+            if(check_8.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Eating too much or too little");
+            }
+            if(check_9.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Smoking or drinking alcohol");
+            }
+            if(check_10.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Being tearful or crying");
+            }
+            if(check_11.isChecked()) {
+                myRef.child("users").child(id).child("Resilience").child("resilience_9").setValue("Restless, like you can't sit still");
+            }
+
+            Toast.makeText(this, "Information Saved", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Slide_12_Behavioural.this, Slide_12_Physical.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/resilience_Screen22.java
+++ app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/resilience_Screen22.java
@@ -3,10 +3,13 @@ package com.example.coachingapp.ResilienceandCopingMechanisms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.coachingapp.R;
+
+import java.io.IOException;
 
 public class resilience_Screen22 extends AppCompatActivity {
 
@@ -15,6 +18,26 @@ public class resilience_Screen22 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resilience__screen22);
     }
+
+
+    public void btn_play(View view) {
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/coachingapp-a95d5.appspot.com/o/guidance%20for%20image%20relaxation.m4a?alt=media&token=81f8aac5-a291-4fde-864f-48b78800d7bf");
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            mediaPlayer.prepare();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public void btn_next(View view) {
         Intent intent = new Intent(resilience_Screen22.this, resilience_Screen23.class );
         startActivity(intent);
