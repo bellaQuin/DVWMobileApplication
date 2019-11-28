fbdc4c0dd5768d7ea262f34d6a30c4a44919c7d6
NonStopPings <henr28@manukaumail.com>
2019-11-28 13:52:25 +1300

    28/11/2019

--- app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/Slide_17_options.java
+++ app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/Slide_17_options.java
@@ -22,7 +22,58 @@ public class Slide_17_options extends AppCompatActivity {
     }
 
     public void btn_Positive_images(View view) {
-        Intent intent = new Intent(Slide_17_options.this, Slide_19.class );
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen20.class );
+        startActivity(intent);
+    }
+
+    public void btn_Music(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen22.class );
+        startActivity(intent);
+    }
+
+    public void btn_Videos(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen22.class );
+        startActivity(intent);
+    }
+
+
+    public void btn_Exercise(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen26.class );
+        startActivity(intent);
+    }
+
+
+
+    public void btn_Stance(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen27.class );
+        startActivity(intent);
+    }
+
+
+
+    public void btn_Gratefulness(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen28.class );
+        startActivity(intent);
+    }
+
+
+    public void btn_Using_nature(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen30.class );
+        startActivity(intent);
+    }
+
+    public void btn_Saying_no(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen31.class );
+        startActivity(intent);
+    }
+
+    public void btn_Meditation_and_Mindfulness(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen34.class );
+        startActivity(intent);
+    }
+
+    public void btn_Purpose_and_meaning(View view) {
+        Intent intent = new Intent(Slide_17_options.this, resilience_Screen35.class );
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/resilience_Screen50.java
+++ app/src/main/java/com/example/coachingapp/ResilienceandCopingMechanisms/resilience_Screen50.java
@@ -2,7 +2,9 @@ package com.example.coachingapp.ResilienceandCopingMechanisms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.coachingapp.R;
 
@@ -12,5 +14,25 @@ public class resilience_Screen50 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resilience__screen50);
+    }
+
+    public void btn_Recognise(View view) {
+        Intent intent = new Intent(resilience_Screen50.this, resilience_Screen51.class );
+        startActivity(intent);
+    }
+
+    public void btn_Response(View view) {
+        Intent intent = new Intent(resilience_Screen50.this, resilience_Screen51.class );
+        startActivity(intent);
+    }
+
+    public void btn_Resolve(View view) {
+        Intent intent = new Intent(resilience_Screen50.this, resilience_Screen51.class );
+        startActivity(intent);
+    }
+
+    public void btn_next(View view) {
+        Intent intent = new Intent(resilience_Screen50.this, resilience_Screen51.class );
+        startActivity(intent);
     }
 }
